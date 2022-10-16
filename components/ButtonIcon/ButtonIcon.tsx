46e5ab90a61f806a35788e82c19c9e520c41bb4e
4ctarus <hatim.hemaidia@gmail.com>
2022-10-16 22:26:01 +0200

    little refacto

--- components/ButtonIcon/ButtonIcon.tsx
+++ components/ButtonIcon/ButtonIcon.tsx
@@ -10,10 +10,8 @@ const hoverStyle =
   'hover:before:block hover:before:absolute hover:before:inset-0 hover:before:rounded-full hover:before:bg-primary hover:before:opacity-[.08]';
 const focusStyle =
   'focus:before:block focus:before:absolute focus:before:inset-0 focus:before:rounded-full focus:before:bg-primary focus:before:opacity-[.12]';
-
 const disabledStyle =
   'disabled:text-on-surface disabled:before:content-none [&>*]:disabled:opacity-[.38]';
-
 const rootClassName = `relative text-primary dark:text-primary-dark box-border inline-flex items-center justify-center h-10 rounded-full w-10 ${hoverStyle} ${focusStyle} ${disabledStyle}`;
 
 function ButtonIcon(props: Props) {
